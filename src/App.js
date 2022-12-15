581c6d97e3011d678daeb1bb42b1ec4d9ff9ca86
Morax997 <71130183+Morax997@users.noreply.github.com>
2022-12-15 18:11:48 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Input, Checkbox} from 'antd';
+import {useState} from "react";
+import {CloseOutlined} from "@ant-design/icons";
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [toDoList, setToDoList]  = useState([]);
+
+  const inputHandler = event => {
+    setInputValue(event.target.value)
+  }
+
+  const onCheckboxChange = (event, elementIndex) => {
+    let temp = toDoList.concat();
+    temp[elementIndex].isChecked = event.target.checked;
+    setToDoList(temp);
+  };
+
+  const submitHandler = () => {
+    setToDoList(toDoList.concat({
+      text: inputValue,
+      isChecked: false,
+    }))
+  }
+
+  const deleteHandler = (deleteIndex) => {
+    let temp = toDoList.concat();
+    temp.splice(deleteIndex, 1);
+    setToDoList(temp)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={"place"}>
+        <Input
+          placeholder="..."
+          onChange={inputHandler}
+          value={inputValue}
+        />
+        <Button onClick={submitHandler}>Submit</Button>
+      </div>
+      {
+        toDoList.map((toDoEntry, index) => {
+          return <div key={index} className={"entryContainer"}>
+            <div className="textContainer">
+              <Checkbox
+                onChange={(event) => {onCheckboxChange(event, index)}}
+                checked={toDoEntry.isChecked}
+              />
+              <div className="text">{toDoEntry.text}</div>
+            </div>
+            <Button
+              icon={<CloseOutlined style={{width: "32px"}} />}
+              onClick={() => {deleteHandler(index)}}
+            />
+          </div>
+        })
+      }
     </div>
   );
 }
